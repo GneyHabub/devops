5219ff5eca3b7231e1391ee3bbf3cd514a67c148
Alexander <mr.afigitelniychuvak@gmail.com>
2021-09-27 13:27:57 +0300

    Lab 12

--- app_python/main.py
+++ app_python/main.py
@@ -3,6 +3,7 @@
 import datetime
 from bottle import route, run, template
 import pytz
+import os
 
 @route('/')
 def msctime():
@@ -20,6 +21,38 @@ def msctime():
       Time in Moscow: &nbsp <b>{{time}}</b>
     </div>
     ''', time=now)
+
+@route('/visits')
+def visits():
+  new_count = 0
+  """Returns the number of visits to the website"""
+  if not os.path.exists("./data"):
+    os.makedirs('data')
+  if not os.path.exists("./data/visitCount.txt"):
+    f = open('./data/visitCount.txt', 'w+')
+    f.write("0")
+    f.close()
+  else:
+    f = open('./data/visitCount.txt', 'r') 
+    data = int(f.read())
+    f.close()
+    new_count = data + 1
+    f = open('./data/visitCount.txt', 'w') 
+    f.write(str(new_count))
+    f.close()
+
+  color = ("00000" + str(hex(new_count))[2:])[-6:]
+  return template('''
+  <div style="
+    display:flex;
+    justify-content:center;
+    font-family:Arial;
+    font-size:40px;
+    padding-top: 10px;
+  ">
+    The website was visited &nbsp <b style="color: {{color}}">{{times}}</b> &nbsp times
+  </div>
+  ''', times=new_count, color="#" + color)
 
 if __name__ == '__main__':
     run(server='paste', host='0.0.0.0', port=8080, debug=False)
